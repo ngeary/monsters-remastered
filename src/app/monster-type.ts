22908d7d636445fa62080cd34fe610bd899dbfce
ngeary <41934692+ngeary@users.noreply.github.com>
2018-12-05 19:16:19 -0800

    Add files via upload

--- src/app/monster-type.ts
+++ src/app/monster-type.ts
@@ -3,6 +3,11 @@ export class MonsterType {
   public id: number;
   public name: string;
   public img: string;
+  public baseMaxHp: number;
+  public baseAttack: number;
+  public baseDefense: number;
+  public baseSpeed: number;
+  public baseDifficulty: number;
 
   constructor() {}
 
--- src/app/monster.ts
+++ src/app/monster.ts
@@ -5,11 +5,24 @@ export class Monster {
     public static monsterCounter = 100;
     public monsterType: MonsterType;
     public serialNum: number;
+    public level: number;
+    public maxHp: number;
+    public curHp: number;
+    public attack: number;
+    public defense: number;
+    public speed: number;
+    public difficulty: number;
 
     constructor(mt: MonsterType) {
         this.monsterType = mt;
-        this.serialNum = Monster.monsterCounter;
-        Monster.monsterCounter++;
+        this.serialNum = Monster.monsterCounter++;
+        this.level = 1;
+        this.maxHp = mt.baseMaxHp;
+        this.curHp = this.maxHp;
+        this.attack = mt.baseAttack;
+        this.defense = mt.baseDefense;
+        this.speed = mt.baseSpeed;
+        this.difficulty = mt.baseDifficulty;
     }
 
 }
